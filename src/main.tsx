9b60b3dfb5655a426db1e99ee76dff6bba792536
MariaJulia <mariajuliadev@gmail.com>
2025-08-23 15:25:09 -0300

    feat: Adiciona estilização do slider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
+import 'swiper/swiper-bundle.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
