b54ba45f5bb1482e96d49f6ceb731a318a6a486d
MariaJulia <mariajuliadev@gmail.com>
2025-08-23 13:47:07 -0300

    feat: Adiciona cursos e slider a seção de minicursos

--- src/components/mini-course/index.tsx
+++ src/components/mini-course/index.tsx
@@ -1,15 +1,57 @@
 import { IoIosArrowForward } from "react-icons/io";
+import { ListMinicourse } from "../../listMinicourse";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 export function Minicourse() {
+    const [sliderPerview, setSliderPerview] = useState<number>(3)
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth < 780) {
+                setSliderPerview(1)
+            } else if (window.innerWidth < 1030) {
+                setSliderPerview(2)
+            } else {
+                setSliderPerview(3)
+            }
+        }
+
+        handleResize()
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
         <section id="minicourse" className="min-h-screen flex items-center justify-center flex-col text-center w-full bg-blue-50/50 px-4 py-12 ">
             <div className="w-full flex items-center flex-col">
-                <h2 className="font-bold bg-gradient-to-l from-blue-900 to-blue-500 bg-clip-text text-transparent md:text-4xl text-2xl font-serif">Minicursos em Destaque</h2>
+                <h2 className="font-bold bg-gradient-to-l from-blue-900 to-blue-500 bg-clip-text text-transparent md:text-4xl text-2xl">Minicursos em Destaque</h2>
                 <p className="text-zinc-700 md:text-lg text-base md:max-w-none max-w-lg">Descubra alguns dos nossos minicursos mais populares e comece sua jornada de aprendizado.</p>
             </div>
+            {/*"bg-gray-300/50 w-full max-w-7xl rounded-xl flex items-center justify-center pt-12 md:px-8 px-0 shadow-md"*/}
+            <div className="w-full max-w-7xl mb-12 py-4">
+                <Swiper
+                    slidesPerView={sliderPerview}
+                    pagination={{ clickable: true }}
+                    navigation
+                    modules={[Navigation, Pagination]}
+                >
 
-            <div className="flex xl:flex-row flex-col gap-12 mb-12">
-                <h3 className="font-medium text-xl py-36 text-blue-950">Agurdando cadastrar cursos.</h3>
+                    {ListMinicourse.map((item) => (
+                        <SwiperSlide key={item.id} className="my-12">
+                            <div className="border-blue-500 border-l-4 bg-blue-400/10 mx-12 rounded-2xl max-w-98 px-4 md:h-73 h-58 flex justify-center text-left flex-col transition-all cursor-pointer shadow-md hover:shadow-2xl">
+                                <span className="text-zinc-700 text-sm">{item.date} • {item.courseLocation}</span>
+                                <h3 className="font-bold md:text-xl text-lg text-blue-950 py-2">{item.title}</h3>
+                                <p className="text-zinc-700 md:text-base text-sm max-w-92">{item.description}</p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
 
             <button className="bg-transparent flex items-center justify-center gap-4 border border-zinc-400 rounded-4xl px-6 h-10 shadow-md text-zinc-700 font-medium cursor-pointer transition-all hover:scale-105">
--- src/listMinicourse.ts
+++ src/listMinicourse.ts
@@ -2,22 +2,35 @@
 interface ListMinicourseProps {
     id: number;
     title: string;
-    instructor1: string;
-    instructor2?: string;
     courseLocation: string;
     date: string;
     hour?: string;
     description: string;
-    instructorImg1?: string;
-    instructorImg2?: string;
+    instructor1: InstructorProps;
+    instructor2?: InstructorProps;
 }
 
-export const listMinicourse: ListMinicourseProps[] = [
+interface InstructorProps {
+    name: string;
+    img?: string;
+    insta?: string;
+    github?: string
+    description?: string;
+}
+
+export const ListMinicourse: ListMinicourseProps[] = [
     {
         id: 1,
         title: 'Conhecendo robótica com LEGO EV3',
-        instructor1: 'João Lucas Galdino Duarte',
-        instructor2: 'Sarah Heloysa Oliveira Tomaz',
+        instructor1: {
+            name: 'João Lucas Galdino Duarte',
+            insta: 'https://www.instagram.com/g4ld1n0_/',
+            github: 'https://github.com/G4ldin0',
+            description: 'Sênior Lego Scratch, desenvolvendo jogos e pra sempre preso entre paginas de documentação ou ando meio desligado.'
+        },
+        instructor2: {
+            name: 'Sarah Heloysa Oliveira Tomaz',
+        },
         courseLocation: 'Lab 6',
         date: '23/10',
         description: 'Introdução a conceitos de robótica, discussão sobre uso dessas ferramentas, e sessão prática de montagem e programação de robôs usando o kit de montagem Lego EV3.'
@@ -25,7 +38,9 @@ export const listMinicourse: ListMinicourseProps[] = [
     {
         id: 2,
         title: 'Primeiros passos com C#',
-        instructor1: 'João Gabriel de Araújo Peixoto',
+        instructor1: {
+            name: 'João Gabriel de Araújo Peixoto'
+        },
         courseLocation: 'Lab 3',
         date: '23/10',
         description: 'Conceitos básicos de C#. Discutindo sua utilidade, vantagens, aplicações no mercado. Conhecendo a sintaxe e estrutura do código.'
@@ -33,8 +48,12 @@ export const listMinicourse: ListMinicourseProps[] = [
     {
         id: 3,
         title: 'Criação de apps com Flutter',
-        instructor1: 'Nickolas Emanuel de Oliveira Silva',
-        instructor2: 'Arthur Peixoto de Almeida',
+        instructor1: {
+            name: 'Nickolas Emanuel de Oliveira Silva'
+        },
+        instructor2: {
+            name: 'Arthur Peixoto de Almeida'
+        },
         courseLocation: 'Lab 7',
         date: '24/10',
         description: 'Conceitos básicos do framework. Conhecendo a estrutura do funcionamento, widgets básicos, formas de processar os dados para criar um aplicativo multiplataforma.'
@@ -42,8 +61,18 @@ export const listMinicourse: ListMinicourseProps[] = [
     {
         id: 4,
         title: 'Introdução ao desenvolvimento de jogos com a Godot Engine 4',
-        instructor1: 'João Lucas Galdino Duarte',
-        instructor2: 'Wesley Felipe Xavier Rocha',
+        instructor1: {
+            name: 'João Lucas Galdino Duarte',
+            insta: 'https://www.instagram.com/g4ld1n0_/',
+            github: 'https://github.com/G4ldin0',
+            description: 'Sênior Lego Scratch, desenvolvendo jogos e pra sempre preso entre paginas de documentação ou ando meio desligado.'
+        },
+        instructor2: {
+            name: 'Wesley Felipe Xavier Rocha',
+            insta: 'https://www.instagram.com/whesxavi/',
+            github: 'GitHub.com/X4vierWhes',
+            description: 'Acho que dá pra rodar DOOM aqui.'
+        },
         courseLocation: 'Lab 2',
         date: '24/10',
         description: 'Conhecendo a estrutura e design da Godot Engine 4. Programando um jogo simples para exercitar a lógica do funcionamento da Engine. Discussão acerca de padrões de design usados para desenvolver jogos na Godot.'
