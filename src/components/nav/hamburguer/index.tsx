0f1e2beafc532df9d2775183e94981bbfe58c156
MariaJulia <mariajuliadev@gmail.com>
2025-08-23 17:12:46 -0300

    feat: Adiciona borda ao hamburguer

--- src/components/nav/hamburguer/index.tsx
+++ src/components/nav/hamburguer/index.tsx
@@ -7,7 +7,7 @@ export function Hamburguer() {
     const [statusNav, setStatusNav] = useState(false)
 
     return (
-        <header className="z-50 fixed top-4 left-4 right-4 md:left-8 md:right-8 lg:left-16 lg:right-16 max-w-5xl mx-auto flex items-center justify-center rounded-full bg-white md:h-16 h-12 border-2 border-zinc-300 shadow-md">
+        <header className=" z-50 fixed top-4 left-4 right-4 md:left-8 md:right-8 lg:left-16 lg:right-16 max-w-5xl mx-auto flex items-center justify-center rounded-full bg-white md:h-16 h-12 border-2 border-zinc-300 shadow-md">
             <nav className="flex justify-between md:gap-0 gap-4 items-center w-full px-12">
                 <a href="#welcome">
                     <img src={capcomLogo} alt="Logo capcom" className="md:w-24 w-18" />
@@ -18,9 +18,9 @@ export function Hamburguer() {
                     </button>
 
                     {statusNav && (
-                        <section className='relative left-5'>
-                            <div className="absolute top-14 right-8 w-4 h-4 bg-white transform rotate-45 shadow-lg"></div>
-                            <nav className="bg-white px-5 py-7 absolute right-5 top-16 z-10 rounded-lg shadow-lg sm:w-64 w-54 flex flex-col justify-center gap-2">
+                        <section className='relative left-5 '>
+                            <div className="border-2 border-zinc-300 absolute top-14 right-8 w-4 h-4 bg-white transform rotate-45 shadow-lg"></div>
+                            <nav className="border-2 border-zinc-300 bg-white px-5 py-7 absolute right-5 top-16 z-10 rounded-lg shadow-lg sm:w-64 w-54 flex flex-col justify-center gap-2">
                                 <a onClick={() => setStatusNav(false)} href="#infos" className="flex items-center justify-between font-medium text-zince-700 md:text-lg text-base transition-all hover:scale-105 hover:text-blue-700">Sobre
                                     <RiArrowRightSLine size={20} />
                                 </a>
