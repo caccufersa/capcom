186132656ff19902c2a969c9b080bbd8d575a1c2
MariaJulia <mariajuliadev@gmail.com>
2025-08-23 16:26:43 -0300

    Troca de fotos do footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,4 @@
-import epcLogo from '../../assets/epcLogo.jpg';
+import epcLogo from '../../assets/epcLogo.png';
 import caccLogo from '../../assets/_Centro Acadêmico de Ciência da Computação.png';
 import { FaInstagram } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
@@ -8,8 +8,8 @@ export function Footer() {
     return (
         <footer className="bg-gradient-to-l from-blue-900 to-blue-500 flex justify-between w-full lg:h-58 h-128 px-12 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] lg:flex-row flex-col">
             <div className='flex flex-1 items-center justify-center gap-6'>
-                <img src={epcLogo} alt="Logo Escola Piloto de Computação" className='lg:w-32 w-22 rounded-full' />
-                <img src={caccLogo} alt="Logo Centro Acadêmico de Ciência da Computação" className='lg:w-32 w-22' />
+                <img src={epcLogo} alt="Logo Escola Piloto de Computação" className='lg:w-20 w-12 rounded-full' />
+                <img src={caccLogo} alt="Logo Centro Acadêmico de Ciência da Computação" className='lg:w-30 w-22' />
             </div>
             <div className='flex flex-col flex-1 items-center justify-center'>
                 <h3 className='text-white font-medium md:text-xl text-lg'>Contato</h3>
