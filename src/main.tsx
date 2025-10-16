bcd498de09e6b505f6686175a781b7191fb09a20
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-10-16 20:33:43 -0300

    feat: add Google Analytics integration with react-ga4 and update .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'swiper/swiper-bundle.css'
-
+import ReactGA from 'react-ga4'; // Importe a biblioteca
+// Use Vite environment variable to store the measurement id securely
+const MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID ?? '';
+if (MEASUREMENT_ID) {
+  ReactGA.initialize(MEASUREMENT_ID);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
